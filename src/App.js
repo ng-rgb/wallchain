84a4a5c4f9e0e3438e83d30a610b6d95fcf5a9d8
robbie <robbiekruszynski@gmail.com>
2022-10-08 17:58:11 -0500

    check env

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import "./App.css";
+import logo from './imgs/logo.png'
+// import TopFold from './components/Topfold/TopFold.js'
 
 import QRScan from "./qr-reader/qr-reader-component";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Wallchain</h1>
+      {/* < TopFold /> */}
       <QRScan />
     </div>
   );
---
+++ src/components/Topfold/Topfold.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./App.css";
+import logo from './imgs/logo.png'
+
+
+function TopFold() {
+  return (
+    <div className="App">
+        return <img src={logo} alt="Logo" />;
+        <h3>immortalizing street art on the blockchain</h3>
+      <h2>Explore the city & scan the murals</h2>
+      
+    </div>
+  );
+}
+
+export default TopFold;
