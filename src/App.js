41ce240d9456da5f836ad7719c95c61dd1076ab3
robbie <robbiekruszynski@gmail.com>
2022-10-08 17:20:20 -0500

    trying to not break everything

--- src/App.js
+++ src/App.js
@@ -1,29 +1,13 @@
-import React, { useState } from "react";
-import { QrReader } from "@blackbox-vision/react-qr-reader";
+import React from "react";
+import "./App.css";
+
+import QRScan from "./qr-reader/qr-reader-component";
 
 export default function App() {
-  const [on, toggle] = useState(true);
-  const [data, setData] = useState("NULL");
-
   return (
     <div className="App">
-      <button onClick={() => toggle(!on)} style={{ marginBottom: 16 }}>
-        {on ? "Unmount QR Reader" : "Mount QR Reader"}
-      </button>
-      {on && (
-        <QrReader
-          resolution={600}
-          facingMode="environment"
-          onScan={decoded => setData(decoded)}
-          onError={err => console.info(err)}
-        />
-      )}
-      <div>
-        <p>
-          El valor del QR es:{" "}
-          {typeof data === "object" ? JSON.stringify(data) : data}
-        </p>
-      </div>
+      <h1>Wallchain</h1>
+      <QRScan />
     </div>
   );
 }
