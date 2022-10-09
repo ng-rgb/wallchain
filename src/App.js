875f72ce7ca0eebddefe1083cc833cdbcbd4a83f
robbie <robbiekruszynski@gmail.com>
2022-10-08 20:57:35 -0500

    add imgs

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
-import React from "react";
-import "./App.css";
-import Top from "./Top/Top"
-import logo from './imgs/logo.png'
-import QRScan from "./qr-reader/qr-reader-component";
+// import React from "react";
+// import "./App.css";
+// import Top from "./Top/Top"
+// import logo from './imgs/logo.png'
+// import QRScan from "./qr-reader/qr-reader-component";
 
-export default function App() {
+// export default function App() {
+//   return (
+//     <div className="App">
+//       <Top />
+//         {/* return <img src={logo} alt="Logo" />;
+//         <h3>immortalizing street art on the blockchain</h3>
+//       <h2>Explore the city & scan the murals</h2> */}
+//       {/* <QRScan /> */}
+//     </div>
+//   );
+// }
+import "./App.css";
+import Cards from "./components/cards";
+
+function App() {
   return (
     <div className="App">
-      <Top />
-        {/* return <img src={logo} alt="Logo" />;
-        <h3>immortalizing street art on the blockchain</h3>
-      <h2>Explore the city & scan the murals</h2> */}
-      {/* <QRScan /> */}
+      <Cards />
     </div>
   );
 }
 
+export default App;
