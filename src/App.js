ee149b8add668a19c6e9a592fdbdb113b23a6e73
robbie <robbiekruszynski@gmail.com>
2022-10-08 22:48:44 -0500

    clean up

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import "./App.css";
+import logo from "./imgs/logo.png"
 import Cards from "./components/Cards";
 import Map from "./components/Map/Map"
-// import 
+// import Intro from "./components/Intro/Intro"
+
 
 function App() {
   return (
     <div className="App">
+      <img src={logo} alt="Logo" />
+      {/* <img src="logo" alt""" */}
+      {/* <Intro /> */}
       <Cards />
       <Map/>
     </div>
